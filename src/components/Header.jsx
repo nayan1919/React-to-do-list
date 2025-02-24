2f77fa3c6cbbbd7d3b4563a805536850ad33d0fd
Nayan-WeatherAPI <duttanayan1919@gmail.com>
2025-02-24 19:09:03 +0530

    added github link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import "./style.css";
 const Header = () => {
     return (
         <header>
+            <a href="https://github.com/nayan1919/React-to-do-list">Git-HubLink</a>
             <h1 className="to">TO DO LIST</h1>
         </header>
     );
